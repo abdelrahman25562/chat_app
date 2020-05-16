4546dfb3f37401ef647cba39f2ae6a8d531380de
Abd-ALrahman <abdelrahman25562@gmail.com>
2020-05-16 02:15:44 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'const.dart';
+import 'login.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat Demo',
+      theme: ThemeData(
+        primaryColor: themeColor,
+      ),
+      home: LoginScreen(title: 'CHAT DEMO'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
